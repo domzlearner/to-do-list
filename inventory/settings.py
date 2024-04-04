d49761a02764b0a8e6901f308920c3cc64e96cb4
Domz Condes <143238561+domzlearner@users.noreply.github.com>
2024-04-04 11:32:10 +0800

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -124,8 +124,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'domz.learner@gmail.com'
-EMAIL_HOST_PASSWORD = 'togm vfjx naws ungj'
+EMAIL_HOST_USER = 'xxxx@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxx'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
